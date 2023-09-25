28b41344219853a0d9e5be3b8f44f979d9efed6a
Chelsea Mogwo <chelseamogwo@gmail.com>
2023-09-25 12:19:56 +0100

    add content to profile page

--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -7,14 +7,16 @@ const Profile = () => {
       {/* <div>Profile</div> */}
       <div id='container'>
         <div id="sidebar">
-          <ul>
-            <li>Profile</li>
-            <li>Settings</li>
-          </ul>
+          <h5>Profile</h5>
+          <a id='sidebar-likes' href="#likes"><h5 >Likes</h5></a>
         </div>
         <div id="main-content">
           <h2>Profile</h2>
-          
+          <h3>Name</h3>
+          <input type="text" placeholder='Name' id="" />
+          <h3>Username</h3>
+          <input type="text" placeholder='Username' id="" />
+          <h3 id='likes'>Likes</h3>
         </div>
       </div>
     </>
--- src/pages/index.jsx
+++
@@ -1,2 +0,0 @@
-export {default as ProfilePage} from './ProfilePage'
-export { default as Homepage } from './Homepage'
