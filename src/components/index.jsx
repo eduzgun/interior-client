8884992332b0dd0b4bd63d342b76c8e303913b23
Franki Callard <francescalily@outlook.com>
2023-09-25 15:40:14 +0100

    making room component work

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,6 @@
+import { fromHalfFloat } from "three/src/extras/DataUtils"
+
 export {default as LoginCard} from "./LoginCard"
 export {default as LightSwitch} from "./LightSwitch"
 export {default as SignupCard} from "./SignupCard"
-
+export { default as Room } from "./Room"
