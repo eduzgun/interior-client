d32ebac990890c2bff2e4c46a2fb6e7f3bb5a12c
Chelsea Mogwo <chelseamogwo@gmail.com>
2023-09-25 17:51:47 +0100

    edit button on homepage

--- src/components/Environment/index.jsx
+++ src/components/Environment/index.jsx
@@ -1,17 +1,10 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { Environment as EnvironmentImpl } from '@react-three/drei'
-import { useFrame } from "@react-three/fiber"
 
 const Environment = () => {
-  const cubeRef = useRef()
-  useFrame((state, delta) => {
-    cubeRef.current.rotation.y += 0.1
-    // console.log(cubeRef.current.rotation.y)
-  })
+
   return (
-    <mesh ref={cubeRef}>
       <EnvironmentImpl preset="city" />
-    </mesh>
     
   )
 }
