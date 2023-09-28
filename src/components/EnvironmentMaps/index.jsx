93a7f8e7af73b002e2882eaab9382eb0e8693071
Franki Callard <francescalily@outlook.com>
2023-09-28 14:53:19 +0100

    getting likes to be sent to the backend with user id and room id

--- src/components/EnvironmentMaps/index.jsx
+++ src/components/EnvironmentMaps/index.jsx
@@ -5,12 +5,35 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import  Marker  from '../Marker';
 import Heart from "react-animated-heart";
+import axios from 'axios'
 
 import { AiOutlineComment } from 'react-icons/ai'
 
 const EnvironmentMap = ({ mapUrls }) => {
   const containerRef = useRef(null);
   const [isClick, setClick] = useState(false);
+
+  const handleLike = async () => {
+    setClick(prev => !prev);
+
+    //I will change this to the actual user logged in just wanted to check that it works first
+    const likeData = {
+        user_id: 2,
+        room_id: 2
+    };
+
+    try {
+        const response = await axios.post('http://localhost:5000/likes', likeData);
+        console.log('Like created', response.data);
+    } catch (error) {
+        console.error('Error creating like:', error);
+
+       
+        setClick(prev => !prev);  
+    }
+}
+
+  
 
   useEffect(() => {
     const container = containerRef.current;
@@ -21,7 +44,7 @@ const EnvironmentMap = ({ mapUrls }) => {
     const textureLoader = new THREE.TextureLoader();
 
   
-    const gui = new dat.GUI();
+    // const gui = new dat.GUI();
     const global = {};
 
     const canvas = document.createElement('canvas');
@@ -65,16 +88,16 @@ const updateAllMaterials = () =>
  scene.backgroundBlurriness = 0 
 scene.backgroundIntensity = 1 
 
-gui.add(scene, 'backgroundBlurriness').min(0).max(1).step(0.001)
-gui.add(scene, 'backgroundIntensity').min(0).max(10).step(0.001)
+// gui.add(scene, 'backgroundBlurriness').min(0).max(1).step(0.001)
+// gui.add(scene, 'backgroundIntensity').min(0).max(10).step(0.001)
 
-global.envMapIntensity = 1
-gui
-    .add(global, 'envMapIntensity')
-    .min(0)
-    .max(10)
-    .step(0.001)
-    .onChange(updateAllMaterials)
+// global.envMapIntensity = 1
+// gui
+//     .add(global, 'envMapIntensity')
+//     .min(0)
+//     .max(10)
+//     .step(0.001)
+//     .onChange(updateAllMaterials)
 
 
     const environmentMap = new THREE.CubeTextureLoader().load(mapUrls);
@@ -115,15 +138,17 @@ gui
 };
   }, [mapUrls]);
 
+  
+
   return (
     <>
   <div ref={containerRef} className="environment-map" />
   {/* <Marker label="1" text="Information text and liking will go here !!!!! Have to make other components" /> */}
   <div className='like-bar'>
         <p className='favourites'>Add to favourites</p>
-         <Heart isClick={isClick} onClick={() => setClick(!isClick)} />
+         <Heart isClick={isClick} onClick={handleLike} />
 
-         <p></p>
+         
          <button className='comments-button'>Comments <AiOutlineComment /></button>
       </div>
   </>
