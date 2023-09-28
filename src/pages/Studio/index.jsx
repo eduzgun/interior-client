1355b9af7b79988b86694134444d9b75571c3314
Franki Callard <francescalily@outlook.com>
2023-09-28 16:31:06 +0100

    setting up other pages

---
+++ src/pages/Studio/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Room } from '../../components'
+
+const Studio= () => {
+  return (
+    <div>
+        <div className='center-page-room'>
+      <Room />
+      </div>
+      
+    </div>
+  );
+};
+
+export default Studio;
