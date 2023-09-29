fc5a163248ad2b6f378bb25c2ff2cefa6a4e8abd
Barbara Fajardo <barbarafajardo92@gmail.com>
2023-09-29 09:54:38 +0100

    ammend tests

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,22 @@
-export { default as Login } from "./Login"
-export { default as Home } from "./Home"
-export { default as Rooms } from "./Rooms"
-export { default as NotFound } from "./NotFound"
+export { default as Login } from "./Login";
+export { default as Home } from "./Home";
+export { default as Rooms } from "./Rooms";
+export { default as NotFound } from "./NotFound";
 
-export { default as GenerateRoom } from "./GenerateRoom"
+export { default as GenerateRoom } from "./GenerateRoom";
 
-export { default as Explore } from "./Explore"
+export { default as Explore } from "./Explore";
 
-export { default as Bedroom } from "./Bedroom"
+export { default as Bedroom } from "./Bedroom";
 
-export {default as ProfilePage} from './ProfilePage'
+export { default as ProfilePage } from "./ProfilePage";
 
-export { default as Studio } from "./Studio"
+export { default as Studio } from "./Studio";
 
-export { default as Kitchen } from "./Kitchen"
+export { default as Kitchen } from "./Kitchen";
 
-export { default as Bathroom } from "./Bathroom"
+export { default as Bathroom } from "./Bathroom";
 
-export { default as Garden } from "./Garden"
+export { default as Garden } from "./Garden";
 
-export { default as Living } from "./Living"
-
+export { default as Living } from "./Living";
