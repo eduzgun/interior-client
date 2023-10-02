a784d4162871d7b72e3d07b87ee7391b979d2841
Franki Callard <francescalily@outlook.com>
2023-10-02 12:16:22 +0100

    comments push

--- src/components/nestedComments/api.js
+++ src/components/nestedComments/api.js
@@ -1,3 +1,9 @@
+const timestamp = Date.now();
+const date = new Date(timestamp);
+const formattedDate = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} on ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+
+console.log(formattedDate);
+
 export const getComments = async () => {
   return [
     {
@@ -6,7 +12,7 @@ export const getComments = async () => {
       username: "Jack",
       userId: "1",
       parentId: null,
-      createdAt: "2021-08-16T23:00:33.010+02:00",
+      createdAt: formattedDate,
     },
     {
       id: "2",
@@ -14,7 +20,7 @@ export const getComments = async () => {
       username: "John",
       userId: "2",
       parentId: null,
-      createdAt: "2021-08-16T23:00:33.010+02:00",
+      createdAt: formattedDate,
     },
     {
       id: "3",
@@ -22,7 +28,7 @@ export const getComments = async () => {
       username: "John",
       userId: "2",
       parentId: "1",
-      createdAt: "2021-08-16T23:00:33.010+02:00",
+      createdAt: formattedDate,
     },
     {
       id: "4",
@@ -30,7 +36,7 @@ export const getComments = async () => {
       username: "John",
       userId: "2",
       parentId: "2",
-      createdAt: "2021-08-16T23:00:33.010+02:00",
+      createdAt: formattedDate,
     },
   ];
 };
