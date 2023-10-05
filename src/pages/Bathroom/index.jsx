3b3c106a358c9209d56e79c364ef483f6e394c9c
emptybagelman <samuelmcgin@aol.com>
2023-10-05 11:14:30 +0100

    added likes across all the sub pages

--- src/pages/Bathroom/index.jsx
+++ src/pages/Bathroom/index.jsx
@@ -1,15 +1,20 @@
-import React, {useState, useEffect} from 'react';
-import { Room, StylesComponent, BackButton } from '../../components'
+import React, {useState, useEffect,useRef } from 'react';
+import { Room, StylesComponent, BackButton,BlobToImage } from '../../components'
 import { Link } from 'react-router-dom';
+import Heart from "react-animated-heart";
+import { AiFillEye } from 'react-icons/ai'
+import './explore.css'
+import { useAuth } from '../../contexts';
 import axios from 'axios';
+import {GrClose} from 'react-icons/gr'
 
-// const bathroomImages = [
-//   { src: '../../src/assets/environmentMaps/5/px.png', alt: 'Image 1' },
-//   { src: '../../src/assets/environmentMaps/5/px.png', alt: 'Image 1' },
-//   { src: '../../src/assets/environmentMaps/5/px.png', alt: 'Image 1' },
-//   { src: '../../src/assets/environmentMaps/5/px.png', alt: 'Image 1' },
-//    { src: '../../src/assets/environmentMaps/5/px.png', alt: 'Image 1' },
-//   { src: '../../src/assets/environmentMaps/5/px.png', alt: 'Image 1' },
+const bathroomImages = [
+  { src: '../../src/assets/environmentMaps/5/px.png', alt: 'Image 1' },
+  { src: '../../src/assets/environmentMaps/5/px.png', alt: 'Image 1' },
+  { src: '../../src/assets/environmentMaps/5/px.png', alt: 'Image 1' },
+  { src: '../../src/assets/environmentMaps/5/px.png', alt: 'Image 1' },
+   { src: '../../src/assets/environmentMaps/5/px.png', alt: 'Image 1' },
+  { src: '../../src/assets/environmentMaps/5/px.png', alt: 'Image 1' },
 //   { src: '../../src/assets/environmentMaps/bedroom/7.webp', alt: 'Image 1' },
 //   { src: '../../src/assets/environmentMaps/bedroom/8.jpeg', alt: 'Image 1' },
 //   { src: '../../src/assets/environmentMaps/bedroom/1.png', alt: 'Image 1' },
@@ -22,17 +27,24 @@ import axios from 'axios';
 //   { src: '../../src/assets/environmentMaps/bedroom/8.jpeg', alt: 'Image 1' },
 
   
-// ];
+];
 
 
 function BathroomPage() {
-    const [imagesWithStyles, setImagesWithStyles] = useState([])
+  const { user } = useAuth();
+  const [hoveredImageIndex, setHoveredImageIndex] = useState(null);
+  const [roomArray,setRoomArray] = useState([])
+  const [likedImages, setLikedImages] = useState(new Array(roomArray.length).fill(false));
+  const [imagesWithStyles, setImagesWithStyles] = useState([])
   const [selectedImage, setSelectedImage] = useState(null);
   const [selectedImageIndex, setSelectedImageIndex] = useState(null);
-  const [roomArray,setRoomArray] = useState([])
 
 
   const handleImageClick = (image, index) => {
+    document.body.style.overflow = 'hidden';
+    const updatedImages = [...imagesWithStyles];
+    //updatedImages[index].clickCount += 1;
+    setImagesWithStyles(updatedImages);
     setSelectedImage(image);
     setSelectedImageIndex(image.id);
   };
@@ -42,17 +54,42 @@ function BathroomPage() {
     setSelectedImageIndex(null)
   };
 
- useEffect(() => {
-
+  useEffect(() => {
     const newImagesWithStyles = roomArray.map((image, index) => ({
         ...image,
-        style: <StylesComponent seed={index + 3} />,
+        clickCount: image.clickCount || 0,  
+        style: <StylesComponent seed={index} />,
     }));
     setImagesWithStyles(newImagesWithStyles);
 }, []);
 
+const toggleLike = async (index) => {
+  const newLikedImages = [...likedImages];
+  newLikedImages[index] = !newLikedImages[index];
+  setLikedImages(newLikedImages);
 
-  useEffect(() => {
+  if (newLikedImages[index]) {
+    const roomId = imagesWithStyles[index].id;
+    await sendLikeData(user, roomId);
+  }
+};
+
+const sendLikeData = async (user, roomId) => {
+  try {
+    const response = await axios.post('http://localhost:5000/likes', { user_id: user, room_id: roomId });
+
+    if (!response.data) {
+      throw new Error('Failed to send data');
+    }
+
+    console.log('Like created', response.data);
+  } catch (error) {
+    console.error("There was an error sending data:", error);
+  }
+};
+
+
+useEffect(() => {
   const handleScroll = (e) => {
     const fullscreenDiv = document.querySelector('.fullscreen-div');
     if (fullscreenDiv) {
@@ -77,11 +114,14 @@ useEffect(() => {
     const call = await axios.get("http://localhost:5000/rooms").then(data => {
       const rooms = data.data.rooms
       const tempArr = []
+      let counter = 0
       for(let i=0;i<rooms.length;i++){
         if(rooms[i].category === "Bathroom"){
-          rooms[i].src = rooms[i].cover_image
+            // rooms[i].src = rooms[i].cover_image
+          rooms[i].src = bathroomImages[counter].src
           rooms[i].alt = 'Image 1'
           tempArr.push(rooms[i])
+          counter += 1
         }
       }
       setRoomArray(tempArr)
@@ -91,33 +131,53 @@ useEffect(() => {
   callRoomImages()
 },[])
 
-  return (
-    <>
-    <div className='title-section'>
-      <h1 className='room-title'>Bathroom Inspiration</h1>
-      <BackButton backTo="/explore" label="Back to Explore" />
+return (
+  <div className='overflow-hiding'>
+      <div className='title-section'>
+    <h1 className='room-title'>Bedroom Inspiration</h1>
+    <BackButton backTo="/explore" label="Back to Explore" />
+    </div>
+  
+  <div className={`bathroom-page${selectedImage ? ' dimmed' : ''}`}>
+    {roomArray.map((image, index) => (
+  <div className="bathroom__item-container" 
+    key={index} 
+    onClick={() => handleImageClick(image, index)}
+    onMouseEnter={() => setHoveredImageIndex(image.id)}
+    onMouseLeave={() => setHoveredImageIndex(null)}
+  >
+  <img className='bathroom__item' src={image.src} alt={image.alt} />
+  <div className="bathroom__item-caption">{image.name}
+  {hoveredImageIndex == image.id && (
+    <div className="icon-container">
+  
+      <div className="heart-container" onClick={(e) => { e.stopPropagation(); toggleLike(image.id); }}>
+        <Heart isClick={likedImages[image.id]} />
       </div>
-    <div className={`bathroom-page${selectedImage ? ' dimmed' : ''}`}>
-      {roomArray.map((image, index) => (
-  <div className="bathroom__item-container" key={index} onClick={() => handleImageClick(image, index)}>
-    <img className='bathroom__item' src={image.src} alt={image.alt} />
-    <div className="bathroom__item-caption">{image.name}</div>
-  </div>
+      
+      <div className="click-count">
+        <AiFillEye />
+        <span> {image.clickCount}</span>
+      </div>
+    </div>
+  )}</div>
+  
+  
+</div>
 ))}
 
-      {selectedImage && (
-        <div className="fullscreen-div">
-        <div className="fullscreen-content">
-            
-            <Room mapSet="bathroom" initialMapIndex={selectedImageIndex} />
-            
-            <button className="close-button" onClick={handleCloseClick}>Close</button>
-        </div>
-    </div>
-      )}
-    </div>
-    </>
-  );
+
+    {selectedImage && (
+      <div className="fullscreen-div">
+      <div className="fullscreen-content">
+          <Room mapSet="bedroom" initialMapIndex={selectedImageIndex} />
+          <button className="close-button" onClick={handleCloseClick}><GrClose /></button>
+      </div>
+  </div>
+    )}
+  </div>
+  </div>
+);
 }
 
 export default BathroomPage;
--- src/pages/Bedroom/index.jsx
+++ src/pages/Bedroom/index.jsx
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 import Heart from "react-animated-heart";
 import { AiFillEye } from 'react-icons/ai'
 import './explore.css'
-import { useAuth } from '../../contexts/index.jsx';
+import { useAuth } from '../../contexts';
 import axios from 'axios';
 import {GrClose} from 'react-icons/gr'
 
  const bedroomImages = [
    { id: 1, src: '../../src/assets/environmentMaps/bedroom/1.png', alt: 'Image 1', clickCount: 0 },
    { id: 2, src: '../../src/assets/environmentMaps/0/1/pz.png', alt: 'Image 1', clickCount: 0},
-  // { id: 3, src: '../../src/assets/environmentMaps/bedroom/2.jpeg', alt: 'Image 1', caption: "Modern", clickCount: 0 },
-  // { id: 4, src: '../../src/assets/environmentMaps/bedroom/3.jpeg', alt: 'Image 1', clickCount: 0 },
-  // { id: 5, src: '../../src/assets/environmentMaps/bedroom/4.jpeg', alt: 'Image 1', clickCount: 0 },
-  // { id: 6, src: '../../src/assets/environmentMaps/bedroom/5.avif', alt: 'Image 1', clickCount: 0 },
-  // { id: 7, src: '../../src/assets/environmentMaps/bedroom/6.jpeg', alt: 'Image 1', clickCount: 0 },
+  { id: 3, src: '../../src/assets/environmentMaps/bedroom/2.jpeg', alt: 'Image 1', caption: "Modern", clickCount: 0 },
+  { id: 4, src: '../../src/assets/environmentMaps/bedroom/3.jpeg', alt: 'Image 1', clickCount: 0 },
+  { id: 5, src: '../../src/assets/environmentMaps/bedroom/4.jpeg', alt: 'Image 1', clickCount: 0 },
+  { id: 6, src: '../../src/assets/environmentMaps/bedroom/5.avif', alt: 'Image 1', clickCount: 0 },
+  { id: 7, src: '../../src/assets/environmentMaps/bedroom/6.jpeg', alt: 'Image 1', clickCount: 0 },
   // { id: 8, src: '../../src/assets/environmentMaps/bedroom/7.jpeg', alt: 'Image 1', clickCount: 0 },
   // { id: 9, src: '../../src/assets/environmentMaps/bedroom/8.webp', alt: 'Image 1', clickCount: 0 },
   // { id: 10, src: '../../src/assets/environmentMaps/bedroom/1.png', alt: 'Image 1', clickCount: 0 },
@@ -30,18 +30,14 @@ import {GrClose} from 'react-icons/gr'
 
 function BedroomPage() {
   const { user } = useAuth();
-    const [hoveredImageIndex, setHoveredImageIndex] = useState(null);
-
-    const [roomArray,setRoomArray] = useState([])
-
-    const [likedImages, setLikedImages] = useState(new Array(roomArray.length).fill(false));
-
-    const [imagesWithStyles, setImagesWithStyles] = useState([])
+  const [hoveredImageIndex, setHoveredImageIndex] = useState(null);
+  const [roomArray,setRoomArray] = useState([])
+  const [likedImages, setLikedImages] = useState(new Array(roomArray.length).fill(false));
+  const [imagesWithStyles, setImagesWithStyles] = useState([])
   const [selectedImage, setSelectedImage] = useState(null);
   const [selectedImageIndex, setSelectedImageIndex] = useState(null);
 
   const handleImageClick = (image, index) => {
-    console.log("line44",image.clickCount);
     document.body.style.overflow = 'hidden';
     const updatedImages = [...imagesWithStyles];
     //updatedImages[index].clickCount += 1;
@@ -71,7 +67,6 @@ const toggleLike = async (index) => {
 
   if (newLikedImages[index]) {
     const roomId = imagesWithStyles[index].id;
-    console.log("line74",roomId);
     await sendLikeData(user, roomId);
   }
 };
@@ -90,27 +85,27 @@ const sendLikeData = async (user, roomId) => {
   }
 };
 
-  useEffect(() => {
-    async function callRoomImages(){
-      const call = await axios.get("http://localhost:5000/rooms").then(data => {
-        const rooms = data.data.rooms
-
-        const tempArr = []
-        for(let i=0;i<rooms.length;i++){
-    
-          if(rooms[i].category === "Bedroom"){
-            rooms[i].src = rooms[i].cover_image
-            rooms[i].alt = 'Image 1'
-            rooms[i].clickCount = 0
-            tempArr.push(rooms[i])
-          }
+useEffect(() => {
+  async function callRoomImages(){
+    const call = await axios.get("http://localhost:5000/rooms").then(data => {
+      const rooms = data.data.rooms
+      const tempArr = []
+      let counter = 0
+      for(let i=0;i<rooms.length;i++){
+        if(rooms[i].category === "Bedroom"){
+            // rooms[i].src = rooms[i].cover_image
+          rooms[i].src = bedroomImages[counter].src
+          rooms[i].alt = 'Image 1'
+          tempArr.push(rooms[i])
+          counter += 1
         }
-        setRoomArray(tempArr)
-      })
+      }
+      setRoomArray(tempArr)
+    })
 
-    }
-    callRoomImages()
-  },[])
+  }
+  callRoomImages()
+},[])
 
 
   useEffect(() => {
